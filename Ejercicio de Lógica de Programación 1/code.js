d76a531a67a3055b6ece419b1d2c510a3e1bcdbd
HugoMorales <hugo.156@hotmail.com>
2023-03-15 20:20:08 -0600

    Update README.me ejercicio 1

--- Ejercicio de Lógica de Programación 1/code.js
+++ Ejercicio de Lógica de Programación 1/code.js
@@ -3,13 +3,34 @@ let num1 = parseFloat(prompt("Introduce el primer número:"));
 let num2 = parseFloat(prompt("Introduce el segundo número:"));
 let num3 = parseFloat(prompt("Introduce el tercer número:"));
 
-// Ordenar los números de mayor a menor y mostrarlos por consola
-console.log("Números ordenados de mayor a menor: " + Math.max(num1, num2, num3) + ", " + Math.min(Math.max(num1, num2), Math.max(num2, num3), Math.max(num1, num3)) + ", " + Math.min(num1, num2, num3));
+// Verificar que los valores introducidos son números
+if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
+  console.log("Por favor, introduce valores numéricos válidos");
+} else {
+  // Ordenar los números de mayor a menor y mostrarlos por consola
+  let numMayor, numCentro, numMenor;
 
-// Ordenar los números de menor a mayor y mostrarlos por consola
-console.log("Números ordenados de menor a mayor: " + Math.min(num1, num2, num3) + ", " + Math.min(Math.max(num1, num2), Math.max(num2, num3), Math.max(num1, num3)) + ", " + Math.max(num1, num2, num3));
+  if (num1 >= num2 && num1 >= num3) {
+    numMayor = num1;
+    numCentro = Math.max(num2, num3);
+    numMenor = Math.min(num2, num3);
+  } else if (num2 >= num1 && num2 >= num3) {
+    numMayor = num2;
+    numCentro = Math.max(num1, num3);
+    numMenor = Math.min(num1, num3);
+  } else {
+    numMayor = num3;
+    numCentro = Math.max(num1, num2);
+    numMenor = Math.min(num1, num2);
+  }
 
-// Mostrar un mensaje si los números son iguales
-if (num1 === num2 && num2 === num3) {
-  console.log("Los tres números son iguales");
+  console.log("Números ordenados de mayor a menor: " + numMayor + ", " + numCentro + ", " + numMenor);
+
+  // Ordenar los números de menor a mayor y mostrarlos por consola
+  console.log("Números ordenados de menor a mayor: " + numMenor + ", " + numCentro + ", " + numMayor);
+
+  // Mostrar un mensaje si los números son iguales
+  if (num1 === num2 && num2 === num3) {
+    console.log("Los tres números son iguales");
+  }
 }
---
+++ Ejercicio de Lógica de Programación 1/readme.md
@@ -0,0 +1,16 @@
+#Descripción del Código
+##Instrucciones
+- El siguiente programa en JavaScript solicita al usuario que ingrese tres números y los almacena en tres variables distintas. A continuación, los ordena de mayor a menor y de menor a mayor, y los muestra en consola.
+
+- Si los tres números son iguales, el programa también muestra un mensaje en consola para informar al usuario.
+
+##Consideraciones
+- Para ejecutar el código es necesario contar con un entorno de desarrollo que permita la ejecución de JavaScript, como un navegador web o un programa especializado.
+
+- Al solicitar los números al usuario, se convierten a tipo float mediante la función parseFloat() para poder realizar comparaciones numéricas más adelante.
+
+- Para verificar que los valores ingresados sean números válidos, se utiliza la función isNaN() para comprobar si alguna de las variables no es un número.
+
+- Se utiliza una serie de condicionales if y else if para determinar cuál de los tres números es el mayor y cuál es el menor. Luego, se asignan a tres variables distintas en orden, utilizando las funciones Math.max() y Math.min() para determinar el número central.
+
+- Finalmente, se muestran los números ordenados de mayor a menor y de menor a mayor en consola, utilizando la función console.log(). Si los tres números son iguales, se muestra un mensaje adicional en consola.
