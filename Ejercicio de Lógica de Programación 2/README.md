64e563e927b4528902ff44b1a72a208c80d61589
HugoMorales <hugo.156@hotmail.com>
2023-03-15 20:32:44 -0600

    Update ejercicio 2

---
+++ Ejercicio de Lógica de Programación 2/README.md
@@ -0,0 +1,9 @@
+# Ejercicio 2
+
+## Descripcion
+
+- Este código de JavaScript solicita al usuario que ingrese una temperatura en grados Celsius y luego convierte esa temperatura a grados Fahrenheit y grados Kelvin. Primero, se almacena la temperatura ingresada por el usuario en la variable "celsius". Luego, se verifica si el valor ingresado es un número utilizando la función "isNaN". Si el valor no es un número, se le solicita al usuario que ingrese un valor numérico válido.
+
+- A continuación, la temperatura en grados Celsius se convierte en grados Fahrenheit utilizando la fórmula (celsius * 1.8) + 32, y se almacena en la variable "fahrenheit". También se convierte la temperatura en grados Celsius a grados Kelvin utilizando la fórmula celsius + 273.15, y se almacena en la variable "kelvin".
+
+- Finalmente, se muestran las temperaturas convertidas en grados Fahrenheit y grados Kelvin en la consola utilizando la función "console.log".
---
+++ Ejercicio de Lógica de Programación 2/code.js
@@ -0,0 +1,17 @@
+// Solicitamos al usuario que ingrese la temperatura en grados Celsius y la almacenamos en la variable "celsius"
+let celsius = parseFloat(prompt("Ingresa la temperatura en grados Celsius:"));
+
+// Verificamos si el valor ingresado es un número, si no lo es, mostramos un mensaje de error y volvemos a solicitar el valor
+while (isNaN(celsius)) {
+  celsius = parseFloat(prompt("Por favor, ingresa un valor numérico para la temperatura en grados Celsius:"));
+}
+
+// Convertimos la temperatura de grados Celsius a grados Fahrenheit y la almacenamos en la variable "fahrenheit"
+let fahrenheit = (celsius * 1.8) + 32;
+
+// Convertimos la temperatura de grados Celsius a grados Kelvin y la almacenamos en la variable "kelvin"
+let kelvin = celsius + 273.15;
+
+// Mostramos por consola la temperatura convertida en grados Fahrenheit y en grados Kelvin
+console.log("Grados Fahrenheit: " + fahrenheit);
+console.log("Grados Kelvin: " + kelvin);
